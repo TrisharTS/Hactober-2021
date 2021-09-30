ffc2b088dd911c1002218dc327072913cffcf6a8
Karthik <53967591+karthik-dinesh@users.noreply.github.com>
2021-09-30 23:39:39 +0530

    used temp variable to swap

--- start.c
+++ start.c
@@ -1,11 +1,12 @@
 #include<stdio.h>  
  int main()    
 {    
-int a=10, b=20;      
+int num1=10, num2=20; 
+int temp;
 printf("Before swap a=%d b=%d",a,b);      
-a=a+b;//a=30 (10+20)    
-b=a-b;//b=10 (30-20)    
-a=a-b;//a=20 (30-10)    
+temp=num1;   
+num1=num2;  
+num2=temp;   
 printf("\nAfter swap a=%d b=%d",a,b);    
 return 0;  
 }   
