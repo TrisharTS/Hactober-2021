db19d9018e982b1cd34ab5c32d2d46e291e06149
PiyushAnand94 <91467145+PiyushAnand94@users.noreply.github.com>
2021-10-03 17:33:56 +0530

    Update start.c
    changed variable

--- start.c
+++ start.c
@@ -1,12 +1,12 @@
 #include<stdio.h>  
  int main()    
 {    
-int n1=10, n2=20; 
+int a=10, b=20; 
 int temp;
 printf("Before swap a=%d b=%d",a,b);      
-temp=n1;   
-n1=n2;  
-n2=temp;   
+temp=a;   
+a=b;  
+b=temp;   
 printf("\nAfter swap a=%d b=%d",a,b);    
 return 0;  
 }   
